ae90d88c6bc9cbfd492dd929ce6546736b6c223f
AlexandrosKap <alexandroskapretsos@gmail.com>
2022-05-30 09:08:20 +0300

    py works now

--- projects/py/README.md
+++ projects/py/README.md
@@ -1,7 +1,6 @@
 # pybiomesdale
 
 Based zoomer language.
-Not working...
 
 ```
 python3 pybiomesdale.py https://www.mediafire.com/file/2taazoznoek6cjn/mods.zip/file
--- projects/py/pybiomesdale.py
+++ projects/py/pybiomesdale.py
@@ -1,41 +1,63 @@
 #!/bin/env python3
 
-# Beta script. Not working...
+# Downdownload zip for Minecaf from MeFire.
 
-import sys
+import glob
 import io
-import re
+import os
 import platform
+import re
 import requests
+import shutil
+import sys
 import zipfile
 
-# Get bytes of zip file.
+# Get bytes of zip file from URL.
 def download(url):
-    exp = "https://download.*.zip"
     match = re.search(
-        exp,
+        "https://download.*.zip",
         requests.get(url).content.decode()
     ).group()
     return requests.get(match).content
 
-# Extract zip file to .minecraft/mods.
-def extract(buffer):
+# Get the mods directory.
+def get_mods_dir():
     mods_dir = os.path.expanduser("~")
     if platform.system() != "Windows":
         mods_dir += "/.minecraft/mods"
     else:
         mods_dir += "\\AppData\\Roaming\\.minecraft\\mods"
+    return mods_dir
+
+# Extract zip file to .minecraft/mods.
+def extract(buffer):
     with zipfile.ZipFile(io.BytesIO(buffer)) as file:
-        file.extractall(mods_dir)
+        file.extractall(get_mods_dir())
+
+# Delete the files in the mods directory.
+def clear():
+    mods_dir = get_mods_dir()
+    if platform.system() != "Windows":
+        mods_dir += "/*"
+    else:
+        mods_dir += "\\*"
+    
+    for file_path in glob.glob(mods_dir):
+        if os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+        else:
+            os.remove(file_path)
 
 def main():
     if len(sys.argv) > 1:
+        url = sys.argv[1]
         try:
+            clear()
             print("Downloading...")
-            buffer = download(sys.argv[1])
+            buffer = download(url)
             print("Extracting...")
             extract(buffer)
-            print("Done. Yeahhh boiiiiii!")
+            print("Done boiiiiii!")
         except:
             print("Something failed. Try again.")
             return 1
